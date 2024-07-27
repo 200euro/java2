16eed22e344d80be1e247f49002260bd014eafc9
200euro <AKMALKADYROV281@GMAIL.COM>
2024-07-27 16:58:42 +0500

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,20 @@
+
+
+let number = prompt('Число')
+
+if (number < 0) {
+   console.log('отрицательно');
+} else {
+   console.log('положительно');
+
+}
+
+
+// let age = 2024
+
+// let number = prompt('год рождения')
+
+
+
+
+// console.log(2024 - number);
